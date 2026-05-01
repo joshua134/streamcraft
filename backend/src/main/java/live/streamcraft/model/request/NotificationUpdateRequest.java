d6a9da061a4f86302bca85910d8fb9db00699c85
joshua134 <isiyejoshua@gmail.com>
2026-05-01 16:34:13 +0300

    feat(request/dto): update NotificationUpdateRequest

--- backend/src/main/java/live/streamcraft/model/request/NotificationUpdateRequest.java
+++ backend/src/main/java/live/streamcraft/model/request/NotificationUpdateRequest.java
@@ -1,11 +1,5 @@
 package live.streamcraft.model.request;
 
-import java.time.LocalDateTime;
-import java.util.Map;
-
-import live.streamcraft.model.enums.NotificationPriority;
-import live.streamcraft.model.enums.NotificationType;
-
 @lombok.Setter
 @lombok.Getter
 @lombok.Builder
